8348432a97da1b4e092b41ae58af519fe0451505
SodaSyrup <salad@bysalad.studio>
2026-03-07 02:00:55 +0100

    adjusted the wool to string recipe and buffed the create version because mally is a crybaby

--- .pakku/overrides/kubejs/server_scripts/wool_to_string.js
+++ .pakku/overrides/kubejs/server_scripts/wool_to_string.js
@@ -1,8 +1,41 @@
 ServerEvents.recipes(event => {
     event.shapeless(
-    	Item.of('minecraft:string', 1),
+    	Item.of('minecraft:string', 2),
           [
    			 '#minecraft:wool'
  		 ]
-    )
+    );
+
+    event.remove({ output: 'minecraft:string', type: 'create:milling' });
+    
+    event.custom({
+        type: 'create:milling',
+        ingredients: [
+            { tag: 'minecraft:wool' }
+        ],
+        processingTime: 200,
+        results: [
+            { item: 'minecraft:string', count: 2 }
+        ]
+    });
+    
+	event.custom({
+        "type": "create:crushing",
+        "ingredients": [
+            {
+                "tag": "minecraft:wool"
+            }
+        ],
+        "processingTime": 100,
+        "results": [
+            {
+                "item": "minecraft:string",
+                "count": 4
+            },
+            {
+                "item": "minecraft:string",
+                "chance": 0.5
+            }
+        ]
+    });
 });
