3f8a13e5e06d30924d37ff1961840c21c24a3b89
Salad <salad@bysalad.studio>
2026-06-07 08:53:04 +0200

    made compass not consume when making maps

---
+++ .pakku/overrides/kubejs/server_scripts/misc/map_no_compass_consume.js
@@ -0,0 +1,14 @@
+ServerEvents.recipes(event => {
+    
+    event.remove({ output: 'minecraft:map' })
+
+    event.shaped('minecraft:map', [
+        'PPP',
+        'PCP',
+        'PPP'
+    ], {
+        P: 'minecraft:paper',
+        C: 'minecraft:compass'
+    }).keepIngredient('minecraft:compass') 
+
+})
